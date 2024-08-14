0d71c2daeb8ef3be8c69b0be3ce76e7922c3314c
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-08-14 13:04:28 +0530

    almost done but small error occred

---
+++ Desktop/full-stack-projects/1.authentication-project/backend/middlewares/verifyToken.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+
+export const verifyToken = (req, res, next) => {
+    
+    console.log("Incoming Request:");
+    console.log("URL:", req.url);
+    console.log("Method:", req.method);
+    console.log("Cookies:", req.cookies);
+
+    const token=req.cookies.token;
+
+    if (!token)
+        return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+
+        if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
+
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        console.log("Error in verifyToken", error);
+        return res.status(500).json({ success: false, message: "Server error" });
+    }
+};
